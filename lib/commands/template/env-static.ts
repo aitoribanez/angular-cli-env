89c3eb5cdb4f9173845b6ace7a0d11873cb968ca
Aitor Ibanez <aitiba@gmail.com>
2016-11-10 01:02:49 +0100

    revert

--- lib/commands/template/env-static.ts
+++
@@ -1,5 +0,0 @@
-import { AppEnv } from './app-env.interface';
-
-export const ENV: AppEnv = {<% _.forEach(env, function(v, k) { %>
-    <%= k %>: <%= _.isString(v) ? "\'"+v+"\'" : v %>,<% }) %>
-};
--- lib/commands/template/env.ts
+++
@@ -1,3 +0,0 @@
-export const ENV = {<% _.forEach(env, function(v, k) { %>
-    <%= k %>: <%= _.isString(v) ? "\'"+v+"\'" : v %>,<% }) %>
-};
