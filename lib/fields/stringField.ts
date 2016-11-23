fe132016a16989171cbb2c6ba15ad258b3564268
Aitor Ibanez <aitiba@gmail.com>
2016-11-23 02:27:04 +0100

    StringField running

--- lib/fields/stringField.ts
+++ lib/fields/stringField.ts
@@ -7,7 +7,27 @@ export class StringField implements FieldI {
     public createdAt: Date,
     public updatedAt: Date) { }
 
-   template() {
-    return 'template';
+  template(entityName, entityNameCapitalize, field) {
+    let args = '';
+    delete field.type;
+    // console.log('arguments', Object.keys(field).length);
+    if (Object.keys(field).length > 0) {
+      // console.log('IF');
+      args = ' ';
+      for (let key in field) {
+        if (field.hasOwnProperty(key)) {
+          args += `${key}="${field[key]}" `;
+        }
+      }
+      args = args.slice(0, -1);
+    }
+
+    return `<div>
+      <label>${entityNameCapitalize}</label>
+      <input type="text" name="${entityName}"${args}>
+    </div>
+    `;
   }
 }
+
+
